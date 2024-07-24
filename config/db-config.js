e4e241c0fbec867483dcca29012fe3bfc1f9f478
ganebabuk <kganeshbabu.it@gmail.com>
2024-07-24 22:32:26 +0530

    changes

--- config/db-config.js
+++ config/db-config.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://<username>:<password>@cluster0.p5xlgf9.mongodb.net/<DB_NAME>?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => {
-  console.log("Connected to Database.");        
-})
-.catch((error) => {
-  console.log("Could not connect to Database.", error);
-});     
+const connectDB = async() => {
+  try {
+    await mongoose.connect("mongodb+srv://<user_name>:<password>@cluster0.p5xlgf9.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=Cluster0");
+    console.log("Connected to the atlas.");    
+  }
+  catch(error) {
+    console.error("Could not connect to theatlas.", error);
+  }; 
+}   
+connectDB(); 
