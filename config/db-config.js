bf172b2f57bccb83b159f7b3b6533f4bfbd2cf29
ganebabuk <kganeshbabu.it@gmail.com>
2024-07-25 12:22:14 +0530

    db changes

--- config/db-config.js
+++ config/db-config.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
-const connectDB = async() => {
+const dbConfiguration = async() => {
   try {
-    await mongoose.connect("mongodb+srv://<user_name>:<password>@cluster0.p5xlgf9.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=Cluster0");
-    console.log("Connected to the atlas.");    
+    await mongoose.connect("mongodb+srv://<username>:<password>@cluster0.p5xlgf9.mongodb.net/<db_name>?retryWrites=true&w=majority&appName=<cluster_name>");
+    console.log("Connected to the atlas cloud."); 
+  } catch(error) {
+    console.error("Could not connect to the atlas cloud. ", error);
   }
-  catch(error) {
-    console.error("Could not connect to the atlas. Error: ", error);
-  }; 
-}   
-connectDB(); 
+}
+dbConfiguration();
