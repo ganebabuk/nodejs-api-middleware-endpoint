791a2117dbaf542f3fa8065078c1567e4dbec6f0
ganebabuk <kganeshbabu.it@gmail.com>
2024-10-29 21:07:31 +0530

    changes

--- app.js
+++ app.js
@@ -644,9 +644,9 @@ app.post('/upload', upload.single('file'), (req, res) => {
   });
 });
 
-// Start the server
-// const PORT = 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
+Start the server
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
